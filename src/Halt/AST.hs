c6bcb1bbfccb93221e8efff4703d9d29bbb59f1b
Luka Horvat <darwin226@gmail.com>
2015-03-17 16:17:57 +0100

    Restructuring into Declaration > Statement > Expression

--- src/Halt/AST.hs
+++ src/Halt/AST.hs
@@ -1,19 +1,20 @@
 module Halt.AST where
 
-data Statement = --     module
-                 Import String
-                 --       module alias
-               | ImportAs String String
-                 --           name   type
-               | FunctionType String TypeLiteral
-                 --           name   args     body
-               | FunctionDecl String [String] Expression
-                 --   name   generics    cases
-                 --                 constr    types
-               | Data String [Char] [(String, [TypeLiteral])]
-                 --     name        fields
-                 --            fieldName fieldType
-               | Record String [(String, TypeLiteral)]
+data Declaration = --     module
+                   Import String
+                   --       module alias
+                 | ImportAs String String
+                   --           name   type
+                 | FunctionType String TypeLiteral
+                   --           name   args     body
+                 | FunctionDecl String [String] Statement
+                   --   name   generics    cases
+                   --                 constr    types
+                 | Data String [Char] [(String, [TypeLiteral])]
+                   --     name        fields
+                   --            fieldName fieldType
+                 | Record String [(String, TypeLiteral)]
+                 deriving Show
 
 data TypeLiteral = --        typeParam
                    Parameter Char
@@ -24,19 +25,27 @@ data TypeLiteral = --        typeParam
                    --       from        to
                  | Function TypeLiteral TypeLiteral
                  | Var
+                 deriving Show
 
-data Expression = --         type        name   value
-                  Assignment TypeLiteral String Expression
-                  -- condition  thenBlock  elseBlock
-                | If Expression Expression (Maybe Expression)
-                  --          function   arguments
-                | FunctionApp Expression [Expression]
-                  --  var    start      end        bound      body
-                | For String Expression Expression Expression Expression
-                  --     value
-                | Return Expression
-                  --    exps
-                | Block [Expression]
+data Statement = --         type        name   value
+                 Assignment TypeLiteral String Expression
+                 -- condition  then      else
+               | If Expression Statement (Maybe Statement)
+                 --  var    start      bound body
+               | For String Expression Bound Statement
+                 --     value
+               | Return Expression
+                 --    exps
+               | Block [Statement]
+                 --        expr
+               | NakedExpr Expression
+               deriving Show
+
+data Bound = Bound Expression (Maybe Expression) deriving Show
+
+data Expression = --          function   arguments
+                  FunctionApp Expression [Expression]
                 | IntLiteral Int
                 | DoubleLiteral Double
                 | StringLiteral String
+                deriving Show
