792f2f0dca7f76675bb7d4c0365af9dd526e82a2
Luka Horvat <darwin226@gmail.com>
2015-03-27 01:38:13 +0100

    Implemented a custom indentation aware parser

--- src/Halt/Parsing/Common.hs
+++ src/Halt/Parsing/Common.hs
@@ -8,16 +8,12 @@ import Text.Parsec hiding (token, State)
     import Text.Parsec.String
 -}
 import Data.Functor.Identity
+import Control.Monad.State
 
-type CharStream s = Stream s Identity Char
+type CharStream s m = Stream s m Char
 
 (<++>) :: Applicative f => f [a] -> f [a] -> f [a]
 (<++>) = liftA2 (++)
 
 (<:>) :: Applicative f => f a -> f [a] -> f [a]
 (<:>) = liftA2 (:)
-
-parseHelper :: Stream s Identity t => Parsec s () a -> s -> a
-parseHelper pars str = case parse pars "" str of
-    Left err -> error $ show err
-    Right a  -> a
---
+++ src/Halt/Parsing/Indent.hs
@@ -0,0 +1,30 @@
+module Halt.Parsing.Indent where
+
+import Halt.AST
+import Halt.Parsing.Common
+import Halt.Parsing.Elements
+import Control.Applicative hiding ((<|>), many, optional)
+import Data.Monoid
+import Control.Monad.State.Class
+import Control.Monad.State
+import Text.Parsec hiding (token, State)
+
+type IndentLevel = Int
+type Parser a = ParsecT String IndentLevel (State IndentLevel) a
+
+parseHelper :: Parser a -> String -> a
+parseHelper pars str = case evalState (runParserT pars 0 "" str) 0 of
+    Left err -> error $ show err
+    Right a  -> a
+
+singleOrBlock :: Parser a -> Parser [a]
+singleOrBlock p = (return <$> p) <|> block
+    where block = modify (+ 1) *> many1 (withIndent p) <* modify (subtract 1)
+          withIndent p' = try $ (char '\n' >> get >>= (`count` char '\t')) *> p'
+
+data Test = Line String | Block [Test] deriving Show
+
+test :: Parser Test
+test = block <|> line
+    where block = Block <$> (word "block" *> singleOrBlock test)
+          line  = Line  <$> lowerIdentifier
