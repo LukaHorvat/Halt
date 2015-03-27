e9d265552c02b79a5159e2ba2c44bd4400318ce9
Luka Horvat <darwin226@gmail.com>
2015-03-27 01:37:48 +0100

    Fixed whitespace parsing

--- src/Halt/Parsing/Elements.hs
+++ src/Halt/Parsing/Elements.hs
@@ -9,10 +9,10 @@ import Data.Char
 import Data.Monoid
 import Data.Maybe
 
-whiteSpace :: CharStream s => Parsec s u ()
-whiteSpace = skipMany (skipMany (oneOf " \t") <|> comment)
+whiteSpace :: CharStream s m => ParsecT s u m ()
+whiteSpace = skipMany (skipMany1 (oneOf " \t") <|> comment)
 
-comment :: CharStream s => Parsec s u ()
+comment :: CharStream s m => ParsecT s u m ()
 comment = delimited commentBody <|> (try (word "--") *> skipMany (noneOf "\n")) <?> "comment"
     where delimited = between (word "{-") (word "-}")
           commentBody = skipMany (delimited commentBody <|> nonComment)
@@ -21,83 +21,83 @@ comment = delimited commentBody <|> (try (word "--") *> skipMany (noneOf "\n")) 
               case mb of Nothing -> void anyChar
                          Just _  -> fail "unexpected end of comment"
 
-word :: CharStream s => String -> Parsec s u String
-word str = string str <* spaces
+word :: CharStream s m => String -> ParsecT s u m String
+word str = string str <* whiteSpace
 
-identifierChar :: CharStream s => Parsec s u Char
+identifierChar :: CharStream s m => ParsecT s u m Char
 identifierChar = letter <|> oneOf "_'" <|> digit
 
-lowerIdentifier :: CharStream s => Parsec s u String
-lowerIdentifier = lower <:> option "" (many identifierChar) <* spaces <?> "lower case identifier"
+lowerIdentifier :: CharStream s m => ParsecT s u m String
+lowerIdentifier = lower <:> option "" (many identifierChar) <* whiteSpace <?> "lower case identifier"
 
-capitalIdentifier :: CharStream s => Parsec s u String
-capitalIdentifier = upper <:> option "" lowerIdentifier <* spaces <?> "upper case identifier"
+capitalIdentifier :: CharStream s m => ParsecT s u m String
+capitalIdentifier = upper <:> option "" lowerIdentifier <* whiteSpace <?> "upper case identifier"
 
-parens :: CharStream s => Parsec s u String -> Parsec s u String
+parens :: CharStream s m => ParsecT s u m String -> ParsecT s u m String
 parens p = between (word "(") (word ")") p
 
-intLiteral :: CharStream s => Parsec s u Integer
+intLiteral :: CharStream s m => ParsecT s u m Integer
 intLiteral = read <$> many1 digit
 
-doubleLiteral :: CharStream s => Parsec s u Double
+doubleLiteral :: CharStream s m => ParsecT s u m Double
 doubleLiteral = read <$> many digit <++> option "" (char '.' <:> many1 digit)
 
 --[taken from Text.Parsec.Token] modified formatting and converted to applicative style
-stringLiteral :: CharStream s => Parsec s u String
+stringLiteral :: CharStream s m => ParsecT s u m String
 stringLiteral = (collect <$> str) <?> "literal string"
     where str     = between (char '"') (char '"' <?> "end of string") (many stringChar)
           collect = fromJust . foldr (<>) (Just "") . map (fmap return)
 
-stringChar :: CharStream s => Parsec s u (Maybe Char)
+stringChar :: CharStream s m => ParsecT s u m (Maybe Char)
 stringChar = (Just <$> stringLetter)
          <|> stringEscape
          <?> "string character"
 
-stringLetter :: CharStream s => Parsec s u Char
+stringLetter :: CharStream s m => ParsecT s u m Char
 stringLetter = satisfy (\c -> (c /= '"') && (c /= '\\') && (c > '\026'))
 
-stringEscape :: CharStream s => Parsec s u (Maybe Char)
+stringEscape :: CharStream s m => ParsecT s u m (Maybe Char)
 stringEscape = char '\\' *> escape
     where escape = (escapeGap *> return Nothing)
                <|> (escapeEmpty *> return Nothing)
                <|> (Just <$> escapeCode)
 
-escapeEmpty :: CharStream s => Parsec s u Char
+escapeEmpty :: CharStream s m => ParsecT s u m Char
 escapeEmpty = char '&'
 
-escapeGap :: CharStream s => Parsec s u Char
+escapeGap :: CharStream s m => ParsecT s u m Char
 escapeGap = many1 space *> (char '\\' <?> "end of string gap")
 
-escapeCode :: CharStream s => Parsec s u Char
+escapeCode :: CharStream s m => ParsecT s u m Char
 escapeCode = charEsc <|> charNum <|> charAscii <|> charControl <?> "escape code"
 
-charControl :: CharStream s => Parsec s u Char
+charControl :: CharStream s m => ParsecT s u m Char
 charControl = char '^' *> (toEnum <$> (subtract $ fromEnum 'A') <$> fromEnum <$> upper)
 
-charNum :: CharStream s => Parsec s u Char
+charNum :: CharStream s m => ParsecT s u m Char
 charNum = toEnum <$> fromInteger <$> num
     where num = decimal
             <|> (char 'o' *> number 8  octDigit)
             <|> (char 'x' *> number 16 hexDigit)
 
-charEsc :: CharStream s => Parsec s u Char
+charEsc :: CharStream s m => ParsecT s u m Char
 charEsc = choice $ map parseEsc escMap
     where parseEsc (c, code) = char c *> return code
 
-charAscii :: CharStream s => Parsec s u Char
+charAscii :: CharStream s m => ParsecT s u m Char
 charAscii = choice $ map parseAscii asciiMap
     where parseAscii (asc, code) = try $ string asc *> return code
 
-decimal :: CharStream s => Parsec s u Integer
+decimal :: CharStream s m => ParsecT s u m Integer
 decimal = number 10 digit
 
-hexadecimal :: CharStream s => Parsec s u Integer
+hexadecimal :: CharStream s m => ParsecT s u m Integer
 hexadecimal = oneOf "xX" *> number 16 hexDigit
 
-octal :: CharStream s => Parsec s u Integer
+octal :: CharStream s m => ParsecT s u m Integer
 octal = oneOf "oO" *> number 8 octDigit
 
-number :: CharStream s => Integer -> Parsec s u Char -> Parsec s u Integer
+number :: CharStream s m => Integer -> ParsecT s u m Char -> ParsecT s u m Integer
 number base baseDigit = do
     digits <- many1 baseDigit
     let n = foldl (\x d -> base * x + toInteger (digitToInt d)) 0 digits
